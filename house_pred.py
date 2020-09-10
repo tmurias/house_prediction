78e30a1407ddf3cb92638b949bfef06e45634074
Taylor Murias <taylor@murias.ca>
2020-09-10 15:55:26 -0700

    Moved code into main function

--- house_pred.py
+++ house_pred.py
@@ -12,6 +12,151 @@ from sklearn.preprocessing import StandardScaler
 from xgboost import XGBRegressor
 
 
+def main():
+    # Read training data, deal with all "NA" values appropriately
+    housing = pd.read_csv("data/train.csv")
+    housing = fix_na_values(housing)
+
+    # Change ordinal categories to numbers
+    housing = quantify_ordinals(housing)
+
+    # Break into training/validation
+    train_set, valid_set = train_test_split(housing, test_size=0.2, random_state=69420)
+
+    # Numerical attributes and categorical attributes
+    num_attrs = ["LotFrontage",
+                 "LotArea",
+                 "MasVnrArea",
+                 "BsmtFinSF1",
+                 "BsmtFinSF2",
+                 "BsmtUnfSF",
+                 "TotalBsmtSF",
+                 "1stFlrSF",
+                 "2ndFlrSF",
+                 "LowQualFinSF",
+                 "GrLivArea",
+                 "BsmtFullBath",
+                 "BsmtHalfBath",
+                 "FullBath",
+                 "HalfBath",
+                 "BedroomAbvGr",
+                 "KitchenAbvGr",
+                 "TotRmsAbvGrd",
+                 "Fireplaces",
+                 "GarageCars",
+                 "GarageArea",
+                 "WoodDeckSF",
+                 "OpenPorchSF",
+                 "EnclosedPorch",
+                 "3SsnPorch",
+                 "ScreenPorch",
+                 "PoolArea",
+                 "MiscVal"]
+    ord_attrs = ["OverallQual",
+                 "OverallCond",
+                 "YearBuilt",
+                 "YearRemodAdd",
+                 "ExterQual",
+                 "ExterCond",
+                 "BsmtQual",
+                 "BsmtCond",
+                 "HeatingQC",
+                 "CentralAir",
+                 "KitchenQual",
+                 "FireplaceQu",
+                 "GarageYrBlt",
+                 "GarageFinish",
+                 "GarageQual",
+                 "GarageCond",
+                 "PavedDrive",
+                 "PoolQC",
+                 "YrSold"]
+    cat_attrs = ["Neighborhood",
+                 "MSSubClass",
+                 "MSZoning",
+                 "Street",
+                 "Alley",
+                 "LotShape",
+                 "LandContour",
+                 "BldgType",
+                 "HouseStyle",
+                 "RoofStyle",
+                 "RoofMatl",
+                 "Exterior1st",
+                 "Exterior2nd",
+                 "MasVnrType",
+                 "Foundation",
+                 "BsmtExposure",
+                 "BsmtFinType1",
+                 "BsmtFinType2",
+                 "Heating",
+                 "Electrical",
+                 "Functional",
+                 "GarageType",
+                 "MiscFeature",
+                 "MoSold",
+                 "SaleType",
+                 "SaleCondition"]
+    all_attrs = num_attrs + ord_attrs + cat_attrs
+
+    # Break datasets into data and labels
+    housing_data = housing[all_attrs].copy()
+    housing_labels = housing["SalePrice"].copy()
+    train_data = train_set[all_attrs].copy()
+    train_labels = train_set["SalePrice"].copy()
+    valid_data = valid_set[all_attrs].copy()
+    valid_labels = valid_set["SalePrice"].copy()
+
+    # Apply transformation pipelines
+    num_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median")),
+                             ("std_scaler", StandardScaler())])
+    cat_pipeline = Pipeline([("imputer", SimpleImputer(strategy="most_frequent")),
+                             ("encoder", OneHotEncoder(handle_unknown="ignore"))])
+    full_pipeline = ColumnTransformer([("num", num_pipeline, num_attrs + ord_attrs),
+                                       ("cat", cat_pipeline, cat_attrs)])
+    full_data_prepared = full_pipeline.fit_transform(housing_data)
+    train_data_prepared = full_pipeline.transform(train_data)
+    valid_data_prepared = full_pipeline.transform(valid_data)
+
+    # Train an xgboost regressor
+    arg1 = sys.argv[1]  # "test" or "validate"
+    boost_reg = XGBRegressor(max_depth=3, learning_rate=0.03, n_estimators=1000)
+
+    if arg1 == "test":
+        boost_reg.fit(full_data_prepared, housing_labels)
+
+        # Load and transform the test data
+        test_housing = pd.read_csv("data/test.csv")
+        test_housing = fix_na_values(test_housing)
+        test_housing = quantify_ordinals(test_housing)
+        test_data = test_housing[all_attrs].copy()
+        test_data_prepared = full_pipeline.transform(test_data)
+        predictions = boost_reg.predict(test_data_prepared)
+        with open("data/output.csv", "w", newline='') as output_file:
+            csv_writer = csv.writer(output_file, delimiter=",")
+            csv_writer.writerow(["Id", "SalePrice"])
+            house_id = 1461
+            for p in predictions:
+                row = [house_id, float(p)]
+                csv_writer.writerow(row)
+                house_id += 1
+    elif arg1 == "validate":
+        boost_reg.fit(train_data_prepared, train_labels)
+
+        # Test on validation set
+        predictions = boost_reg.predict(valid_data_prepared)
+
+        # MSLE throws an error if there are negative values
+        for i in range(len(predictions)):
+            if predictions[i] < 0:
+                predictions[i] = 0
+
+        # Evaluate root-mean-squared-log-error
+        lin_msle = mean_squared_log_error(valid_labels, predictions)
+        lin_rmsle = np.sqrt(lin_msle)
+        print("RMSLE for validation data: ", lin_rmsle)
+
+
 def fix_na_values(housing):
     h = housing.copy()
     h = h.fillna(0)
@@ -89,145 +234,5 @@ def quantify_ordinals(housing):
     return h
 
 
-# Read training data, deal with all "NA" values appropriately
-housing = pd.read_csv("data/train.csv")
-housing = fix_na_values(housing)
-
-# Change ordinal categories to numbers
-housing = quantify_ordinals(housing)
-
-# Break into training/validation
-train_set, valid_set = train_test_split(housing, test_size=0.2, random_state=69420)
-
-# Numerical attributes and categorical attributes
-num_attrs = ["LotFrontage",
-             "LotArea",
-             "MasVnrArea",
-             "BsmtFinSF1",
-             "BsmtFinSF2",
-             "BsmtUnfSF",
-             "TotalBsmtSF",
-             "1stFlrSF",
-             "2ndFlrSF",
-             "LowQualFinSF",
-             "GrLivArea",
-             "BsmtFullBath",
-             "BsmtHalfBath",
-             "FullBath",
-             "HalfBath",
-             "BedroomAbvGr",
-             "KitchenAbvGr",
-             "TotRmsAbvGrd",
-             "Fireplaces",
-             "GarageCars",
-             "GarageArea",
-             "WoodDeckSF",
-             "OpenPorchSF",
-             "EnclosedPorch",
-             "3SsnPorch",
-             "ScreenPorch",
-             "PoolArea",
-             "MiscVal"]
-ord_attrs = ["OverallQual",
-             "OverallCond",
-             "YearBuilt",
-             "YearRemodAdd",
-             "ExterQual",
-             "ExterCond",
-             "BsmtQual",
-             "BsmtCond",
-             "HeatingQC",
-             "CentralAir",
-             "KitchenQual",
-             "FireplaceQu",
-             "GarageYrBlt",
-             "GarageFinish",
-             "GarageQual",
-             "GarageCond",
-             "PavedDrive",
-             "PoolQC",
-             "YrSold"]
-cat_attrs = ["Neighborhood",
-             "MSSubClass",
-             "MSZoning",
-             "Street",
-             "Alley",
-             "LotShape",
-             "LandContour",
-             "BldgType",
-             "HouseStyle",
-             "RoofStyle",
-             "RoofMatl",
-             "Exterior1st",
-             "Exterior2nd",
-             "MasVnrType",
-             "Foundation",
-             "BsmtExposure",
-             "BsmtFinType1",
-             "BsmtFinType2",
-             "Heating",
-             "Electrical",
-             "Functional",
-             "GarageType",
-             "MiscFeature",
-             "MoSold",
-             "SaleType",
-             "SaleCondition"]
-all_attrs = num_attrs + ord_attrs + cat_attrs
-
-# Break datasets into data and labels
-housing_data = housing[all_attrs].copy()
-housing_labels = housing["SalePrice"].copy()
-train_data = train_set[all_attrs].copy()
-train_labels = train_set["SalePrice"].copy()
-valid_data = valid_set[all_attrs].copy()
-valid_labels = valid_set["SalePrice"].copy()
-
-# Apply transformation pipelines
-num_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median")),
-                         ("std_scaler", StandardScaler())])
-cat_pipeline = Pipeline([("imputer", SimpleImputer(strategy="most_frequent")),
-                         ("encoder", OneHotEncoder(handle_unknown="ignore"))])
-full_pipeline = ColumnTransformer([("num", num_pipeline, num_attrs + ord_attrs),
-                                   ("cat", cat_pipeline, cat_attrs)])
-full_data_prepared = full_pipeline.fit_transform(housing_data)
-train_data_prepared = full_pipeline.transform(train_data)
-valid_data_prepared = full_pipeline.transform(valid_data)
-
-# Train an xgboost regressor
-arg1 = sys.argv[1]  # "test" or "validate"
-boost_reg = XGBRegressor(max_depth=3, learning_rate=0.03, n_estimators=1000)
-
-if arg1 == "test":
-    boost_reg.fit(full_data_prepared, housing_labels)
-
-    # Load and transform the test data
-    test_housing = pd.read_csv("data/test.csv")
-    test_housing = fix_na_values(test_housing)
-    test_housing = quantify_ordinals(test_housing)
-    test_data = test_housing[all_attrs].copy()
-    test_data_prepared = full_pipeline.transform(test_data)
-    predictions = boost_reg.predict(test_data_prepared)
-    with open("data/output.csv", "w", newline='') as output_file:
-        csv_writer = csv.writer(output_file, delimiter=",")
-        csv_writer.writerow(["Id", "SalePrice"])
-        house_id = 1461
-        for p in predictions:
-            row = [house_id, float(p)]
-            csv_writer.writerow(row)
-            house_id += 1
-elif arg1 == "validate":
-    boost_reg.fit(train_data_prepared, train_labels)
-
-    # Test on validation set
-    predictions = boost_reg.predict(valid_data_prepared)
-
-    # MSLE throws an error if there are negative values
-    for i in range(len(predictions)):
-        if predictions[i] < 0:
-            predictions[i] = 0
-
-    # Evaluate root-mean-squared-log-error
-    lin_msle = mean_squared_log_error(valid_labels, predictions)
-    lin_rmsle = np.sqrt(lin_msle)
-    print("RMSLE for validation data: ", lin_rmsle)
+if __name__ == "__main__":
+    main()
